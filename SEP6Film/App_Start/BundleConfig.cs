64e6c03a4bc2bced718832111f7f22577c0f1bd7
MohawkPigeon <42830998+MohawkPigeon@users.noreply.github.com>
2021-06-03 19:40:47 +0200

    Last update

--- SEP6Film/App_Start/BundleConfig.cs
+++ SEP6Film/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace SEP6Film
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-mint.css",
                       "~/Content/site.css"));
         }
     }
